dec3e3fab9cab49d87b07e8819b71c168c989b7b
Cesar-eav <cesar.eav@hotmail.es>
2024-05-07 10:47:53 +1200

    Portada

--- resources/js/Components/Sidebar/SidebarContent.vue
+++ resources/js/Components/Sidebar/SidebarContent.vue
@@ -5,7 +5,7 @@
         class="relative flex flex-col flex-1 max-h-full gap-4 px-3"
     >
         <SidebarLink
-            title="Dashboard"
+            title="Roman Seguros"
             :href="route('dashboard')"
             :active="route().current('dashboard')"
         >
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -3,18 +3,18 @@
         <template #header>
             <div class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
                 <h2 class="text-xl font-semibold leading-tight">
-                    Dashboard
+                    Roman Seguros
                 </h2>
 
-                <Button external variant="black" target="_blank" class="items-center gap-2 max-w-xs" v-slot="{ iconSizeClasses }" href="https://github.com/kamona-wd/kui-laravel-jetstream">
+                <!-- <Button external variant="black" target="_blank" class="items-center gap-2 max-w-xs" v-slot="{ iconSizeClasses }" href="https://github.com/kamona-wd/kui-laravel-jetstream">
                     <GithubIcon aria-hidden="true" :class="iconSizeClasses" />
                     <span>Star on Github</span>
-                </Button>
+                </Button> -->
             </div>
         </template>
 
         <div class="p-6 flex flex-col gap-6 overflow-hidden bg-white rounded-md shadow-md lg:flex-row md:justify-between dark:bg-dark-eval-1">
-            <div>You're logged in!</div>
+            <div>Estás logeado!</div>
 
             <div>
                 <a 
