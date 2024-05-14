10f70659d60cdbf477ad59bece99e1af29b1be02
Cesar-eav <cesar.eav@hotmail.es>
2024-05-14 09:34:59 -0400

    Tabla polizas

---
+++ database/migrations/2024_05_13_183458_polizas.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Polizas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nombre_de_tabla', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipo_poliza')->nullable();
+            $table->string('numero_poliza')->nullable();
+            $table->decimal('monto_asegurado', 10, 2)->nullable();
+            $table->decimal('prima', 10, 2)->nullable();
+            $table->decimal('valor_neto', 10, 2)->nullable();
+            $table->date('fecha_inicio')->nullable();
+            $table->date('fecha_vencimiento')->nullable();
+            $table->decimal('deducible', 10, 2)->nullable();
+            $table->integer('cantidad_cuotas')->nullable();
+            $table->string('dia_pago')->nullable();
+            $table->string('metodo_pago')->nullable();
+            $table->string('aseguradora')->nullable();
+            $table->string('vendedor')->nullable();
+            $table->string('archivos_seleccionados')->nullable(); // Para almacenar los nombres de los archivos seleccionados
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('polizas');
+
+    }
+}
