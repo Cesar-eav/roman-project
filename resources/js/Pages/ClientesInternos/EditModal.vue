7df161c212f0328dfc7a2ec7935648b6573e2dbd
Cesar-eav <cesar.eav@hotmail.es>
2024-05-24 13:05:39 -0400

    Modal cierra

--- resources/js/Pages/ClientesInternos/EditModal.vue
+++ resources/js/Pages/ClientesInternos/EditModal.vue
@@ -6,60 +6,72 @@
             </h2>
         </div>
         <form @submit.prevent="submit">
-
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6 text-slate-900 mx-4">
-            <div>
-                <label for="nivel_ejecutivo">Nivel Ejecutivo:</label>
-                <input type="text" v-model="formCliente.cargo" :placeholder="formCliente.cargo" class="custom-input" /> 
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6 text-slate-900 mx-4">
+                <div>
+                    <label for="nivel_ejecutivo">Nivel Ejecutivo:</label>
+                    <input type="text" v-model="formCliente.cargo" :placeholder="formCliente.cargo"
+                        class="custom-input" />
+                </div>
+                <div>
+                    <label for="name">Nombre:</label>
+                    <input type="text" v-model="formCliente.name" :placeholder="formCliente.name"
+                        class="custom-input" />
+                </div>
+                <div>
+                    <label for="last_name">Apellido:</label>
+                    <input type="text" v-model="formCliente.last_name" :placeholder="formCliente.last_name"
+                        class="custom-input" />
+                </div>
+                <div>
+                    <label for="rut">RUT:</label>
+                    <input type="text" v-model="formCliente.rut" :placeholder="formCliente.rut" class="custom-input" />
+                </div>
+                <div>
+                    <label for="fecha_nacimiento">Fecha de Nacimiento:</label>
+                    <input type="date" v-model="formCliente.fecha_nacimiento"
+                        :placeholder="formCliente.fecha_nacimiento" class="custom-input" />
+                </div>
+                <div>
+                    <label for="telefono">Teléfono:</label>
+                    <input type="text" v-model="formCliente.telefono" :placeholder="formCliente.telefono"
+                        class="custom-input" />
+                </div>
+                <div>
+                    <label for="email">Correo Electrónico:</label>
+                    <input type="email" v-model="formCliente.email" :placeholder="formCliente.email"
+                        class="custom-input" />
+                </div>
+                <div>
+                    <label for="address">Dirección:</label>
+                    <input type="text" v-model="formCliente.address" :placeholder="formCliente.address"
+                        class="custom-input" />
+                </div>
+                <div>
+                    <label for="ciudad">Ciudad:</label>
+                    <input type="text" v-model="formCliente.ciudad" :placeholder="formCliente.ciudad"
+                        class="custom-input" />
+                </div>
+                <div>
+                    <label for="region">Región:</label>
+                    <input type="text" v-model="formCliente.region" :placeholder="formCliente.region"
+                        class="custom-input" />
+                </div>
+                <div>
+                    <label for="isapre">Isapre:</label>
+                    <input type="text" v-model="formCliente.isapre" :placeholder="formCliente.isapre"
+                        class="custom-input" />
+                </div>
+                <div>
+                    <label for="afp">AFP:</label>
+                    <input type="text" v-model="formCliente.afp" :placeholder="formCliente.afp" class="custom-input" />
+                </div>
             </div>
-            <div>
-                <label for="name">Nombre:</label>
-                <input type="text" v-model="formCliente.name" :placeholder="formCliente.name" class="custom-input" />
+            <div class="flex justify-end mt-2 mr-10">
+                <Button type="button" @keydown.enter="editUser()">
+                    <input type="submit" value="Guardar" title="Guardar" @click="editUser()"
+                        @keydown.enter="editUser()" />
+                </Button>
             </div>
-            <div>
-                <label for="last_name">Apellido:</label>
-                <input type="text" v-model="formCliente.last_name" :placeholder="formCliente.last_name" class="custom-input" />
-            </div>
-            <div>
-                <label for="rut">RUT:</label>
-                <input type="text" v-model="formCliente.rut" :placeholder="formCliente.rut" class="custom-input" />
-            </div>
-            <div>
-                <label for="fecha_nacimiento">Fecha de Nacimiento:</label>
-                <input type="date" v-model="formCliente.fecha_nacimiento" :placeholder="formCliente.fecha_nacimiento" class="custom-input" />
-            </div>
-            <div>
-                <label for="telefono">Teléfono:</label>
-                <input type="text" v-model="formCliente.telefono" :placeholder="formCliente.telefono" class="custom-input" />
-            </div>
-            <div>
-                <label for="email">Correo Electrónico:</label>
-                <input type="email" v-model="formCliente.email" :placeholder="formCliente.email" class="custom-input" />
-            </div>
-            <div>
-                <label for="address">Dirección:</label>
-                <input type="text" v-model="formCliente.address" :placeholder="formCliente.address" class="custom-input" />
-            </div>
-            <div>
-                <label for="ciudad">Ciudad:</label>
-                <input type="text" v-model="formCliente.ciudad" :placeholder="formCliente.ciudad" class="custom-input" />
-            </div>
-            <div>
-                <label for="region">Región:</label>
-                <input type="text" v-model="formCliente.region" :placeholder="formCliente.region" class="custom-input" />
-            </div>
-            <div>
-                <label for="isapre">Isapre:</label>
-                <input type="text" v-model="formCliente.isapre" :placeholder="formCliente.isapre" class="custom-input" />
-            </div>
-            <div>
-                <label for="afp">AFP:</label>
-                <input type="text" v-model="formCliente.afp" :placeholder="formCliente.afp" class="custom-input" />
-            </div>
-        </div>
-        <button type="button"  @keydown.enter="editUser()" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-            <input type="submit" value="Guardar" title="Guardar" @click="editUser()" @keydown.enter="editUser()" />
-        </button>
         </form>
         <div class="px-6 py-4 text-right bg-white dark:bg-dark-eval-3">
             <slot name="footer"></slot>
@@ -140,6 +152,7 @@ export default {
                 })
                 .then((response) => {
                     console.log("RESPUESTA EDICION BACK: ", response.data);
+                    this.$emit('close');
 
                     if (response.data) {
                         console.log("DATOS BACK: ", response.data);
@@ -160,24 +173,25 @@ export default {
 </script>
 
 <style>
-
 .custom-input {
-  margin-top: 0.25rem;
-  margin-right: 0.25rem;
-  display: block;
-  border-color: #d1d5db;
-  border-radius: 0.375rem;
-  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
-  transition: border-color 0.3s, box-shadow 0.3s;
-  padding: 0.5rem; /* Añadir un padding para mejor apariencia */
-  width: 100%; /* Asegurar que el input ocupe todo el ancho disponible */
-  box-sizing: border-box; /* Incluir padding y borde en el ancho total */
+    margin-top: 0.25rem;
+    margin-right: 0.25rem;
+    display: block;
+    border-color: #d1d5db;
+    border-radius: 0.375rem;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+    transition: border-color 0.3s, box-shadow 0.3s;
+    padding: 0.5rem;
+    /* Añadir un padding para mejor apariencia */
+    width: 100%;
+    /* Asegurar que el input ocupe todo el ancho disponible */
+    box-sizing: border-box;
+    /* Incluir padding y borde en el ancho total */
 }
 
 .custom-input:focus {
-  border-color: #f97316;
-  box-shadow: 0 0 0 3px rgba(199, 210, 254, 0.5);
-  outline: none;
+    border-color: #f97316;
+    box-shadow: 0 0 0 3px rgba(199, 210, 254, 0.5);
+    outline: none;
 }
-
 </style>
