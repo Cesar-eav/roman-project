30ce25de3f6d504dd18d2c887c03728ed6ac6437
Cesar-eav <cesar.eav@hotmail.es>
2024-06-17 00:17:24 -0400

    Formato archivo Excel

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -3,11 +3,18 @@
 namespace App\Exports;
 
 use App\Models\User;
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Style\Border;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class UsersExport implements FromCollection, WithHeadings, WithMapping
+class UsersExport implements FromCollection, WithHeadings, WithMapping, WithEvents, WithStyles
 {
     /**
      * @return \Illuminate\Support\Collection
@@ -59,6 +66,54 @@ class UsersExport implements FromCollection, WithHeadings, WithMapping
             $user->isapre,
             $user->afp
 
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                $event->sheet->getStyle('A1:K1')->applyFromArray([
+                    'font' => [
+                        'bold' => true,
+                        'color' => ['argb' => 'FFFFFFFF'],
+                    ],
+                    'fill' => [
+                        'fillType' => Fill::FILL_SOLID,
+                        'color' => ['argb' => 'F97316'],
+                    ],
+                ]);
+
+                $event->sheet->getDelegate()->getStyle('A2:K'.$event->sheet->getDelegate()->getHighestRow())
+                    ->applyFromArray([
+                        'borders' => [
+                            'allBorders' => [
+                                'borderStyle' => Border::BORDER_THIN,
+                                'color' => ['argb' => 'FF000000'],
+                            ],
+                        ],
+                        'alignment' => [
+                            'horizontal' => Alignment::HORIZONTAL_LEFT,
+                        ],
+                    ]);
+
+                            // Ajustar el ancho de las columnas automáticamente
+                foreach (range('A', 'K') as $columnID) {
+                    $event->sheet->getDelegate()->getColumnDimension($columnID)->setAutoSize(true);
+                }
+            },
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style the first row as bold text.
+            1 => ['font' => ['bold' => true]],
+            // Styling a specific cell by coordinate.
+            // 'A1' => ['font' => ['italic' => true]],
+            // Styling an entire column.
+            // 'A'  => ['font' => ['bold' => true]],
         ];
     }
 }
