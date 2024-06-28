21e22301bbb45ac6c0cb32eebce98f8d7c6a467f
Cesar-eav <cesar.eav@hotmail.es>
2024-06-28 13:12:26 -0400

    Nuevo diseño

--- resources/js/Pages/Polizas/CrearCotizacion.vue
+++ resources/js/Pages/Polizas/CrearCotizacion.vue
@@ -1,59 +1,77 @@
 <template>
     <AppLayout title="Dashboard">
         <div>
-            <h1>Crear Cotización</h1>
+            <h1 class="text-2xl ">Crear Cotización</h1>
 
             <!-- Selección del formulario -->
-            <label for="formulario-select">Seleccione un formulario:</label>
             <select v-model="selectedFormulario" id="formulario-select">
+                <option value="">Seleccionar formulario</option>
                 <option v-for="formulario in formularios" :key="formulario.id" :value="formulario">
                     {{ formulario.name_poliza_form }}
                 </option>
             </select>
 
-            <!-- Selección de compañías -->
-            <h2>Seleccione las Compañías de Seguro:</h2>
-            <div v-for="compania in companias" :key="compania.id">
-                <input type="checkbox" :id="'compania-' + compania.id" :value="compania" v-model="selectedCompanias"
-                    @change="updateSelectedEjecutivos(compania)" />
-                <label :for="'compania-' + compania.id" class="text-2xl">{{ compania.razon_social }}</label>
+            <div class="flex mt-5 bg-slate-200 p-10">
+                <!-- Columna de Compañías -->
+                <div class="w-1/2 pr-2">
+                    <h2 class="font-bold	">Seleccione las Compañías de Seguro:</h2>
+                    <div v-for="compania in companias" :key="compania.id" class="mb-2">
+                        <input type="checkbox" :id="'compania-' + compania.id" :value="compania"
+                            v-model="selectedCompanias" @change="updateSelectedEjecutivos(compania)" />
+                        <label :for="'compania-' + compania.id" class="text-sm"> {{ compania.razon_social }}</label>
+                    </div>
+                </div>
 
-                <!-- Ejecutivos asociados a la compañía seleccionada -->
-                <div class="bg-slate-300" v-if="isSelected(compania)">
-                    <h3>Seleccione un Ejecutivo de {{ compania.razon_social }}:</h3>
-                    <div v-for="ejecutivo in compania.ejecutivas" :key="ejecutivo.id">
-                        <input type="radio" :name="'ejecutivo-' + compania.id" :id="'ejecutivo-' + ejecutivo.id"
-                            :value="ejecutivo" v-model="selectedEjecutivos[compania.id]" />
-                        <label :for="'ejecutivo-' + ejecutivo.id">{{ ejecutivo.name }}</label>
+                <!-- Columna de Ejecutivos -->
+                <div class="w-1/2 pl-2">
+                    <h2 class="font-bold	">Seleccione un Ejecutivo:</h2>
+                    <div v-for="compania in selectedCompanias" :key="compania.id" class="mb-2">
+                        <div>
+                            <h3>Ejecutivos de <span class="font-bold">{{ compania.razon_social }}:</span></h3>
+                            <div class="bg-slate-300 rounded-lg p-2">
+                                <div v-for="ejecutivo in compania.ejecutivas" :key="ejecutivo.id">
+                                    <input type="radio" :name="'ejecutivo-' + compania.id"
+                                        :id="'ejecutivo-' + ejecutivo.id" :value="ejecutivo"
+                                        v-model="selectedEjecutivos[compania.id]" />
+                                    <label :for="'ejecutivo-' + ejecutivo.id"> {{ ejecutivo.name }}</label>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
 
+
             <!-- Botón para agregar un nuevo vehículo -->
 
             <!-- Campos del formulario para cada vehículo -->
-            <div v-if="shouldShowForm" class="flex justify-end">
+            <div v-if="shouldShowForm" class="flex justify-end mt-2">
                 <div>
-                    <button class="btn-add-vehicle" @click="addVehicle">Agregar (+)</button>
+                    <button class="btn-add-vehicle" @click="addVehicle">Agregar (+) L</button>
                 </div>
             </div>
 
-         <!-- Campos del formulario para cada vehículo -->
-         <div v-if="shouldShowForm" v-for="(vehicle, vehicleIndex) in vehicles" :key="vehicleIndex"
-                class="vehicle-form grid grid-cols-8  bg-slate-500 ">
-                <h2 class="col-span-8 bg-slate-600">Campos del Formulario para Vehículo {{ vehicleIndex + 1 }}</h2>
-
-                <div v-for="(campo, index) in vehicle.campos" :key="index">
-                    <label :for="campo.id">{{ campo.label }}:</label>
-                    <input :type="campo.type" :id="campo.id" v-model="campo.value" />                    
+            <div v-if="shouldShowVehiculoPesado" class="flex justify-end">
+                <div>
+                    <button class="btn-add-vehicle" @click="addVehicle">Agregar (+) P</button>
                 </div>
-                <button class="btn-remove col-span-1" @click="removeVehicle(vehicleIndex)">(-)</button>
-
-
             </div>
+
+            <!-- Campos del formulario para cada vehículo -->
+            <div v-if="shouldShowForm" v-for="(vehicle, vehicleIndex) in vehicles" :key="vehicleIndex"
+                class="vehicle-form flex flex-wrap bg-slate-200 rounded-lg p-4 mb-4">
+                <div v-for="(campo, index) in vehicle.campos" :key="index" >
+                    <label :for="campo.id" class="block">{{ campo.label }}:</label>
+                    <input :type="campo.type" :id="campo.id" v-model="campo.value" class="w-full" />
+                </div>
+                <div class="w-full flex justify-end">
+                    <button class="btn-remove" @click="removeVehicle(vehicleIndex)">(-)</button>
+                </div>
+            </div>
+
 
             <!-- Botón de envío -->
-            <button @click="submitForm">Enviar</button>
+            <button @click="submitForm" class="btn-generar text-xl">Generar</button>
         </div>
     </AppLayout>
 </template>
@@ -79,13 +97,26 @@ export default {
             vehicles: [
                 {
                     campos: [
-                        { id: 'marca', label: 'Marca', type: 'text', value: '' },
+                        { id: 'marca', label: 'Marca', type: 'text', value: '', placeholder: 'Marca' },
                         { id: 'modelo', label: 'Modelo', type: 'text', value: '' },
                         { id: 'agnio', label: 'Año', type: 'text', value: '' },
                         { id: 'patente', label: 'Patente', type: 'text', value: '' },
                         { id: 'n_chasis', label: 'Nº Chasis', type: 'text', value: '' },
                         { id: 'n_motor', label: 'Nº Motor', type: 'text', value: '' },
                         { id: 'color', label: 'Color', type: 'text', value: '' },
+                    ]
+                }
+
+            ],
+            FormVehiclesPesado: [
+                {
+                    campos: [
+                        { id: 'marca', label: 'Marca', type: 'text', value: '' },
+                        { id: 'modelo', label: 'Modelo', type: 'text', value: '' },
+                        { id: 'agnio', label: 'Año', type: 'text', value: '' },
+                        { id: 'patente', label: 'Patente', type: 'text', value: '' },
+                        { id: 'monto_asegurado', label: 'Monto Ahasis', type: 'text', value: '' }
+
                     ]
                 }
             ]
@@ -190,5 +221,16 @@ export default {
 
 .btn-remove:hover {
     background-color: #da190b;
+}
+
+.btn-generar {
+    background-color: #45a049;
+    color: white;
+    padding: 8px 16px;
+    margin-right: 8px;
+    border: none;
+    cursor: pointer;
+    border-radius: 4px;
+
 }
 </style>
