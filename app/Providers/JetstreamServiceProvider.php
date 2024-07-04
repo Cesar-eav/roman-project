e2a1108651e9e2c2a256152e625b225e530d7ebc
Cesar-eav <cesar.eav@hotmail.es>
2024-07-04 17:15:40 -0400

    Crear Ejectuvos desde teams

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -58,10 +58,31 @@ class JetstreamServiceProvider extends ServiceProvider
             'delete',
         ])->description('Administrator users can perform any action.');
 
-        Jetstream::role('editor', 'Editor', [
+        // Jetstream::role('editor', 'Editor', [
+        //     'read',
+        //     'create',
+        //     'update',
+        // ])->description('Editor users have the ability to read, create, and update.');
+
+        Jetstream::role('ejecutivo', 'Cliente Interno - CREAR EJECUTIVO/A', [
             'read',
             'create',
             'update',
-        ])->description('Editor users have the ability to read, create, and update.');
+        ])->description('Los ejecutivos son de tipo Comercial, Cobraza o Gestor de Siniestros.');
+
+
+        // Jetstream::role('ext-admin', 'Cliente Externo  - ADMIN', [
+        //     'read',
+        //     'create',
+        //     'update',
+        // ])->description('Editor users have the ability to read, create, and update.');
+
+        // Jetstream::role('ext-operario', 'Cliente Externo  - OPERARIO', [
+        //     'read',
+        //     'create',
+        //     'update',
+        // ])->description('Editor users have the ability to read, create, and update.');
+
+
     }
 }
